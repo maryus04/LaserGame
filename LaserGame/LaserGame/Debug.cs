5af9254274fa5c727d6d0dee9ee0748a2f094cf5
Marius  Palade <marius.palade@napa.fi>
2015-03-26 13:32:41 +0200

    Add debug class

---
+++ LaserGame/LaserGame/Debug.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Shapes;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows;
+
+namespace Client {
+    class Debug {
+
+        public static List<Point> pointsToBeDrawn = new List<Point>();
+
+        public static void DrawPoint(Canvas canvas, Point point) {
+            var ellipse = new Ellipse() { Width = 8, Height = 8, Fill= new SolidColorBrush( Colors.Blue ), Stroke = new SolidColorBrush( Colors.Blue ) };
+            Canvas.SetLeft( ellipse, point.X-4 );
+            Canvas.SetTop( ellipse, point.Y-4 );
+            canvas.Children.Add( ellipse );
+        }
+
+    }
+}
