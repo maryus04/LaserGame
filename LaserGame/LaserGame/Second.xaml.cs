8c29b640ebd532f8cdf992cee7e91e10c4c09631
Marius  Palade <marius.palade@napa.fi>
2015-03-26 13:34:33 +0200

    WIP second window

---
+++ LaserGame/LaserGame/Second.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Client {
+    public partial class Second : Window {
+        public Second() {
+            InitializeComponent();
+        }
+
+        public Second(int width,int height) {
+            InitializeComponent();
+            GlobalVariable.debugMode = true;
+            Client.LaserComponents.Laser laser = new Client.LaserComponents.Laser( Canvas );
+
+            laser.buildFirstLine();
+
+
+            //Image im = new Image();
+
+            //var uriSource = new Uri( @"Resources/SpriteMaps/test.png", UriKind.Relative );
+
+            //im.Source = new BitmapImage(uriSource);
+            //Canvas.Children.Add( im );
+            this.Show();
+
+        }
+    }
+}
