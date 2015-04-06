eb84e08d6d5641841ec5993a64af2df652d8e3f2
Marius  Palade <marius.palade@napa.fi>
2015-04-06 11:48:13 +0300

    Add ServerClient.cs

---
+++ LaserGame/Server/ServerClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Server {
+    public class ServerClient {
+        public TcpClient TcpClient {set; get;}
+
+        public StreamReader Reader { set; get; }
+        public StreamWriter Writer { set; get; }
+
+        public string NickName {set; get;}
+
+        public NetworkStream GetStream() {
+            return TcpClient.GetStream();
+        }
+
+        public bool Connected() {
+            return TcpClient.Connected;
+        }
+
+        public String ReadLine() {
+            return Reader.ReadLine();
+        }
+
+        public void Flush() {
+            Writer.Flush();
+        }
+    }
+}
