17b9b4fedbbefb57ac2381a04f985e24f2668a88
Marius  Palade <marius.palade@napa.fi>
2015-04-07 12:00:24 +0300

    Remove unnecessary methods

--- LaserGame/Server/Server.cs
+++ LaserGame/Server/Server.cs
@@ -12,69 +12,32 @@ namespace Server {
         public static Hashtable _nickName;
         public static Hashtable _nickNameByConnect;
 
+        public static void Main() {
+            new Server();
+        }
+
         public Server() {
             _nickName = new Hashtable( 100 );
             _nickNameByConnect = new Hashtable( 100 );
             _chatServer = new TcpListener( IPAddress.Parse( "127.0.0.1" ) , 4296 );
 
-
-            GlobalVariable.debugMode = true;
+            GlobalVariable.debugMode = true; //debug mode
 
             while(true) {
                 _chatServer.Start();
                 if(_chatServer.Pending()) {
-                    TcpClient chatConnection = _chatServer.AcceptTcpClient();
+                    TcpClient connection = _chatServer.AcceptTcpClient();
                     Console.WriteLine( "A new client has connected." );
-                    Communication comm = new Communication( chatConnection );
+
+                    Thread startCommunication = new Thread(() => newCommunication(connection));
+                    startCommunication.Name = "StartConnetion";
+                    startCommunication.Start();
                 }
             }
         }
 
-        public static void Main() {
-            new Server();
-        }
-
-        public static void SendMsgToAll( string nickName, String msg ) {
-            StreamWriter write;
-            ArrayList ToRemove = new ArrayList( 0 );
-            TcpClient[] tcpClient = new TcpClient[Server._nickName.Count];
-            Server._nickName.Values.CopyTo( tcpClient, 0 );
-            for(int count = 0; count < tcpClient.Length; count++) {
-                try {
-                    if(msg.Trim() == "" || tcpClient[count] == null)
-                        continue;
-                    write = new StreamWriter( tcpClient[count].GetStream() );
-                    write.WriteLine( nickName + ":" + msg );
-                    write.Flush();
-                    write = null;
-                } catch(Exception ) {
-                    string str = (string)Server._nickNameByConnect[tcpClient[count]];
-                    Server.SendSysMsg( "*** " + str + " *** left the chat" );
-                    Server._nickName.Remove( str );
-                    Server._nickNameByConnect.Remove( tcpClient[count] );
-                }
-            }
-        }
-
-        public static void SendSysMsg(String msg ) {
-            StreamWriter write;
-            ArrayList ToRemove = new ArrayList( 0 );
-            TcpClient[] tcpClient = new TcpClient[Server._nickName.Count];
-            Server._nickName.Values.CopyTo( tcpClient, 0 );
-            for(int count = 0; count < tcpClient.Length; count++) {
-                try {
-                    if(msg.Trim() == "" || tcpClient[count] == null)
-                        continue;
-                    write = new StreamWriter( tcpClient[count].GetStream() );
-                    write.WriteLine("System: " + msg );
-                    write.Flush();
-                    write = null;
-                } catch(Exception ) {
-                    string str = (string)Server._nickNameByConnect[tcpClient[count]];
-                    Server._nickName.Remove( str );
-                    Server._nickNameByConnect.Remove( tcpClient[count] );
-                }
-            }
+        private void newCommunication(TcpClient connection){
+            new Communication( connection );
         }
     }
 }
