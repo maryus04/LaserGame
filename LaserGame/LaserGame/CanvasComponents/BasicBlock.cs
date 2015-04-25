26ed1032fdbf06c03788e298ef92a85df8d15344
Marius  Palade <marius.palade@napa.fi>
2015-04-25 16:49:15 +0300

    Add class for blocks behavior

---
+++ LaserGame/LaserGame/CanvasComponents/BasicBlock.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Shapes;
+using System.Windows.Media;
+
+namespace Client.CanvasComponents {
+    class BasicBlock {
+
+        private bool CanPass { get; set; }
+        private Rectangle Rectangle { get; set; }
+
+        public BasicBlock( int width, int height, Brush color ) {
+            Rectangle = new Rectangle {
+                Width = width,
+                Height = height,
+                Stroke = color,
+                StrokeThickness = 10
+            };
+        }
+    }
+}
