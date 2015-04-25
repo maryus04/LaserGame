6fd0b1cce594437ebcd4b3057267ba6861a00463
Marius  Palade <marius.palade@napa.fi>
2015-04-25 17:08:48 +0300

    Add properties to BasicBlock

--- LaserGame/LaserGame/CanvasComponents/BasicBlock.cs
+++ LaserGame/LaserGame/CanvasComponents/BasicBlock.cs
@@ -4,20 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Windows.Shapes;
 using System.Windows.Media;
+using System.Windows.Controls;
 
 namespace Client.CanvasComponents {
     class BasicBlock {
 
-        private bool CanPass { get; set; }
-        private Rectangle Rectangle { get; set; }
+        public bool CanPass { get; set; }
+        public Rectangle Rectangle { get; set; }
 
-        public BasicBlock( int width, int height, Brush color ) {
+        public BasicBlock( int width, int height, int leftPossition, int topPossition, Brush color ) {
             Rectangle = new Rectangle {
                 Width = width,
                 Height = height,
                 Stroke = color,
                 StrokeThickness = 10
             };
+            Canvas.SetLeft( Rectangle, leftPossition );
+            Canvas.SetTop( Rectangle, topPossition );
         }
     }
 }
