81b1a15c88fc8d9d56ac340fe647e3f0a9cd4fe7
Marius  Palade <marius.palade@napa.fi>
2015-04-25 17:09:45 +0300

    Add SolidBlock

---
+++ LaserGame/LaserGame/CanvasComponents/SolidBlock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media;
+
+namespace Client.CanvasComponents {
+    class SolidBlock : BasicBlock {
+
+        public SolidBlock( int width, int height, int leftPossition, int topPossition, Brush color )
+            : base( width, height, leftPossition, topPossition, color ) {
+            base.CanPass = true;
+        }
+
+    }
+}
