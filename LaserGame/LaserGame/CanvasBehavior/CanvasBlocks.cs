de2a5c4256540342c1c4cadc2fa1abdbc648d6cd
Marius  Palade <marius.palade@napa.fi>
2015-05-01 17:12:10 +0300

    Add class containing all canvas blocks

---
+++ LaserGame/LaserGame/CanvasBehavior/CanvasBlocks.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Shapes;
+using System.Windows.Media;
+using System.Collections;
+
+namespace Client.CanvasBehavior {
+    static class CanvasBlocks {
+        public static List<Block> list = new List<Block>();
+
+    }
+}
